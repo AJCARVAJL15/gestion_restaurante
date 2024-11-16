38751ccc65858175a98f820ae1acbe09e2347e63
alvaro carvajal <alvarojosecarvajaluribe8@gmail.com>
2024-11-15 21:48:05 -0500

    Se incopora cross origin para el consumo de servicios

--- src/main/java/com/edu/usbcali/gestion_restaurante/controller/SedeController.java
+++ src/main/java/com/edu/usbcali/gestion_restaurante/controller/SedeController.java
@@ -3,6 +3,7 @@ package com.edu.usbcali.gestion_restaurante.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/sede")
+@CrossOrigin("*")
 public class SedeController {
 
     private final SedeRepository sedeRepository;
